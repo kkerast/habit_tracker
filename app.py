76bd2f3463a989d63c6c707289f62e11793379b9
kkerast <dlwlsgkqkq@naver.com>
2023-03-28 00:52:24 +0900

    add_haibt_first_update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
+import datetime
 from pymongo import MongoClient
-client = MongoClient('각자의 db 주소') #각자의 DB 주소를 넣어주세요
-db = client.dbsparta
+client = MongoClient('mongodb+srv://sparta:test@cluster0.9fktzhz.mongodb.net/?retryWrites=true&w=majority') #각자의 DB 주소를 넣어주세요
+db = client.toy_project.habit #각자의 DB 및 collection
 
 
 @app.route('/')
@@ -13,19 +13,20 @@ def home():
 
 
 
-@app.route("/guestbook", methods=["POST"])
+@app.route("/addHabit", methods=["POST"])
 def guestbook_post():
-
+    userid_receive = request.form['User_ID_give']
+    todo_receive = request.form['TODO_give']
+    print(todo_receive,userid_receive)
+    doc = {
+        'userid' : userid_receive,
+        'todo' : todo_receive,
+        'DATE' : datetime.datetime.now()
+    }
+    
+    db.insert_one(doc)
     return jsonify({'result': 'result 완료!'})
 
-
-
-
-@app.route("/guestbook", methods=["GET"])
-def guestbook_get():
-
-
-    return jsonify({'result': 'result'})
 
 
 
